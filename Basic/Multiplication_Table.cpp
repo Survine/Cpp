ba603dd8fa98aa87bcd38a4384fc0b5e8e90d578
Survine <hridaybardhan18@gmail.com>
2024-08-10 14:59:33 +0530

    commit

---
+++ Basic/Multiplication_Table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the Table Number: ";
+    cin >> num;
+    for(int i=1; i<=10; i++)
+    {
+        cout << num <<" x " << i << " = " <<num*i << endl;
+    }
+}
