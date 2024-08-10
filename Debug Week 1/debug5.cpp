d9e43c661ea62bfdd3edfb842af6f0b1a02788f1
Survine <hridaybardhan18@gmail.com>
2024-08-10 22:45:31 +0530

    commit

---
+++ Debug Week 1/debug5.cpp
@@ -0,0 +1,25 @@
+/*
+Pattern
+N = 4
+1
+21
+321
+4321
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, n=4;
+    for (i = 0; i < n; i++)
+    {
+        int p=4;
+        for (j = 0; j < i+1; j++)
+        {
+            cout << p-j;
+            p--;
+        }
+        cout << endl;
+    }
+}
