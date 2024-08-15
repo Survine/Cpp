040027c6405b26f9c3bef61029f11fa24d009396
Survine <hridaybardhan18@gmail.com>
2024-08-15 20:16:04 +0530

    commit

--- Patterns/Numeric_Star_Pattern_1.cpp
+++ Patterns/Numeric_Star_Pattern_1.cpp
@@ -3,5 +3,41 @@ using namespace std;
 
 int main()
 {
-    int
+    int n = 4;
+
+    // First pyramid
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            if (j % 2 == 0)
+            {
+                cout << i + 1;
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
+    // Inverted pyramid
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < 2 * (n - i) - 3; j++)
+        {
+            if (j % 2 == 0)
+            {
+                cout << n - 1 - i;
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
