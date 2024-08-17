90183b52abf7a4865270adc64600f888de387f45
Survine <hridaybardhan18@gmail.com>
2024-08-17 10:20:01 +0530

    commit

---
+++ Patterns/Butterfly_Pattern.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 7;
+    int n = num / 2;
+    //First Half
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "* ";
+        }
+
+        for(int k=0; k< 2*(n-i)-1; k++)
+        {
+            cout << "  ";
+        }
+
+        for(int m=0; m<i+1; m++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    cout <<endl;
+    for(int i=0; i<n; i++)
+    {
+        for(int k=0; k<n; k++)
+        {
+            cout << "* ";
+        }
+    //Second Half
+    for(int i=0; i<n; i++)
+    {
+        for(int k=0; k<n-i; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
