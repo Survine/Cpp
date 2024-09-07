a223cdd4d650eb300a49225d553a2bdb392c6455
Survine <hridaybardhan18@gmail.com>
2024-09-07 18:41:07 +0530

    commit

--- 1D-Array/Two_Sum.cpp
+++ 1D-Array/Two_Sum.cpp
@@ -6,7 +6,7 @@ void Pairs(int arr[], int n)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = i+1; j < n; j++)
         {
             cout << arr[i] << "," << arr[j] << " -> " << arr[i] + arr[j] << endl;
         }
